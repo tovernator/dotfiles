b0e15381f5aa64c1ed2f04e9bbb64dfc5075c7be
tovernator <auke.tegel@protonmail.com>
2026-08-05 14:57:47 +0200

    remove niri output template

--- run_once_before_f_niri_json.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-niri &
-niri msg -j outputs > $HOME/.local/share/chezmoi/outputs.json
-echo "{\"niri_outputs\":  $(niri msg -j outputs)}" > $HOME/.local/share/chezmoi/.chezmoidata/outputs.json
-killall niri
