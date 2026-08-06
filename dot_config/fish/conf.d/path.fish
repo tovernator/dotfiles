2b4a81ca7831f264567d99f324a2b84157c8a361
tovernator <auke.tege@protonmail.com>
2026-08-06 22:06:04 +0200

    Add laptop lid events

--- dot_config/fish/conf.d/path.fish
+++ dot_config/fish/conf.d/path.fish
@@ -7,3 +7,8 @@ end
 if not contains "$HOME/.cargo/bin" $PATH
     set -x PATH "$HOME/.cargo/bin" $PATH
 end
+
+# Mise shims
+if not contains "$HOME/.local/share/mise/shims" $PATH
+    set -x PATH "$HOME/.local/share/mise/shims" $PATH
+end
